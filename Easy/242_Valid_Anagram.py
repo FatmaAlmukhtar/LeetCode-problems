b61104c0a2e4c82fa371c34b5f2bc90872e71190
Fatma Almukhtar <fatma.almukhtar18@gmail.com>
2022-03-24 11:03:52 -0700

    String

---
+++ Easy/242_Valid_Anagram.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+
+        if len(s) != len(t):
+            return False
+
+        lst_t = list(t)
+
+        for i in s:
+            if i not in lst_t:
+                return False
+            else:
+                lst_t.remove(i)
+
+        return True
+
+
+if __name__ == "__main__":
+    assert Solution().isAnagram('car','rat') == False
+    assert Solution().isAnagram('anagram','nagaram') == True
+    assert Solution().isAnagram('aacc','ccac') == False
