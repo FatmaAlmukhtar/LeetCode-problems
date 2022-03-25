899fc7993471e88f1a95aac90b77fb5f92f2cc64
Fatma Almukhtar <fatma.almukhtar18@gmail.com>
2022-03-25 11:50:16 -0700

    Binary Tree, DFS, BFS

---
+++ Easy/617_Merge_Two_Binary_Trees.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def mergeTrees(self, root1, root2):
+        """
+        :type root1: TreeNode
+        :type root2: TreeNode
+        :rtype: TreeNode
+        """
+        if not root1 and not root2:
+            return None
+
+        if root1:
+            val1 = root1.val
+        else:
+            val1 = 0
+
+
+        if root2:
+            val2 = root2.val
+        else:
+            val2 = 0
+
+        merged = TreeNode(val1+val2)
+
+        merged.left = self.mergeTrees(root1.left if root1 else None, root2.left if root2 else None)
+        merged.right = self.mergeTrees(root1.right if root1 else None, root2.right if root2 else None)
+
+
+        return merged
+            
