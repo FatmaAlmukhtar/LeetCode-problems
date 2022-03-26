d4852fa4ed218cb1fba55b82fc0cc62467d26cf3
Fatma Almukhtar <fatma.almukhtar18@gmail.com>
2022-03-26 15:27:11 -0700

    Backtracking, Dynamic Programming

---
+++ Medium/22_Generate_Parentheses.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+
+        res = []
+        stack = []
+
+        def backtracking(open, closed):
+            if open == closed == n:
+                res.append(''.join(stack))
+                return
+
+            if open < n:
+                stack.append('(')
+                backtracking(open+1, closed)
+                stack.pop()
+
+            if closed < open:
+                stack.append(')')
+                backtracking(open, closed+1)
+                stack.pop()
+
+        backtracking(0,0)
+
+        return res
