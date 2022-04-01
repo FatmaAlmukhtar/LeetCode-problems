567694802bfea56450cd206b0debac22e6a8cc44
Fatma Almukhtar <fatma.almukhtar18@gmail.com>
2022-04-01 12:27:45 -0700

    Linked List, Hash Table

---
+++ Medium/142_Linked_List_Cycle_II.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def detectCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        visited = set()
+
+        while head:
+            if head in visited:
+                return head
+
+            visited.add(head)
+            head = head.next
+
+        return None
