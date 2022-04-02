a7cdb1f9cf5ff75a782039a8f96d1930719976c2
Fatma Almukhtar <fatma.almukhtar18@gmail.com>
2022-04-02 16:49:33 -0700

    Dynamic Programming

---
+++ Easy/509_Fibonacci_Number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+
+        dp = [i for i in range(n + 1)]
+
+        for i in range(2,n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+
+        return dp[n]
