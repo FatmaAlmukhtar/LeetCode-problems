b61283f0211ee3d8d00df31166b56ddca603e361
Fatma Almukhtar <fatma.almukhtar18@gmail.com>
2022-04-21 16:32:29 -0700

    String

---
+++ Easy/28_Implement_strStr.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if needle == "":
+            return 0
+
+        l = len(needle)
+
+        for i in range(len(haystack)):
+            if haystack[i] == needle[0]:
+                if haystack[i:i+l] == needle:
+                    return i
+
+        return -1
