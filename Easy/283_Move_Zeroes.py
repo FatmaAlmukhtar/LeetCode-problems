67c22f8533b20617bf97b2bb122a61b6733927e4
Fatma Almukhtar <fatma.almukhtar18@gmail.com>
2022-04-21 21:18:24 -0700

    Two Pointers

---
+++ Easy/283_Move_Zeroes.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        p = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[p] = nums[p], nums[i]
+                p += 1
