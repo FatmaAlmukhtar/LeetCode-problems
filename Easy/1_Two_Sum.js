4e292b42ee9ba5219c3f857b6c3176b4a1cc762c
Fatma Almukhtar <fatmaalmukhtar@Fatmas-MacBook-Air.local>
2022-07-01 11:13:17 -0600

    Hash Table

---
+++ Easy/1_Two_Sum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let visited = new Map();
+    for(let i=0; i<nums.length; i++) {
+        diff = target - nums[i];
+        if (visited.has(diff)) {
+            return [i, visited.get(diff)];
+        }
+        visited.set(nums[i], i);
+    }
+};
